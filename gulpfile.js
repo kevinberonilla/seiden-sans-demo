60b3d49b817e93a9e97ee901106a1b54fb6b1c5f
Kevin Beronilla <kevin.beronilla@gmail.com>
2016-09-14 18:38:55 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    sass = require('gulp-sass'),
+    cssnano = require('gulp-cssnano'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions', '> 5%', 'ie 9'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('cssnano', function() {
+    return gulp.src(['css/**/*.css', '!css/**/*.min.css'])
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(cssnano())
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('uglify', function() {
+    return gulp.src(['js/**/*.js', '!js/**/*.min.js'])
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('js'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('scss/**/*.scss', ['sass']);
+    gulp.watch(['css/**/*.css', '!css/**/*.min.css'], ['cssnano']);
+    gulp.watch(['js/**/*.js', '!js/**/*.min.js'], ['uglify']);
+});
+
+gulp.task('default', ['sass', 'cssnano', 'uglify', 'watch']);
