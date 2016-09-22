1cb9b71b520476b7cd53e3739bf06ec0c477a832
Kevin Beronilla <kevin.beronilla@gmail.com>
2016-09-22 07:53:41 -0500

    Continued styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,56 @@
-// Place your project-specific scripts here
+'use strict';
+
+$(document).ready(function() {
+    /* ----------------------------------------
+    Shared Variables
+    ---------------------------------------- */
+    var documentBody = $(document.body),
+        window = $(window),
+        view = $('html, body');
+    
+    /* ----------------------------------------
+    Web Font Functions
+    ---------------------------------------- */
+    var documentFonts = document.fonts;
+    
+    if (typeof(documentFonts) !== 'undefined') {
+        documentFonts.ready.then(function() {
+            documentBody.addClass('fonts-loaded');
+        });
+    }
+    
+    /* ----------------------------------------
+    User Text Functions
+    ---------------------------------------- */
+    var userText = $('#user-text'),
+        defaultVal = userText.val();
+    
+    function autoResize() {
+        userText.innerHeight(0);
+        
+        var contentHeight = userText[0].scrollHeight;
+        
+        userText.innerHeight(contentHeight);
+    }
+    
+    userText.keyup(autoResize);
+    window.resize(autoResize);
+    
+    userText.focus()
+        .text(defaultVal);
+    
+    /* ----------------------------------------
+    Anchor Link Functions
+    ---------------------------------------- */
+    var anchorLink = $('a[href*="#"]');
+    
+    anchorLink.click(function(e) {
+        e.preventDefault();
+        
+        var targetPos = $($(this).attr('href')).offset().top;
+        
+        view.animate({
+            scrollTop: targetPos
+        }, 1000);
+    });
+});
