a02b244ce94c56c1b09ad8726e4ffb03a5e647b1
Kevin Beronilla <kevin.beronilla@gmail.com>
2017-07-24 11:31:12 -0500

    Edited variable declaration syntax and added explicit paths in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-var gulp = require('gulp'),
-    sourcemaps = require('gulp-sourcemaps'),
-    sass = require('gulp-sass'),
-    cssnano = require('gulp-cssnano'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    autoprefixer = require('gulp-autoprefixer'),
-    runSequence = require('run-sequence');
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var runSequence = require('run-sequence');
 
 gulp.task('sass', function() {
-    return gulp.src('scss/**/*.scss')
+    return gulp.src('./scss/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'expanded'
@@ -18,27 +18,27 @@ gulp.task('sass', function() {
             cascade: false
         }))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('./css'));
 });
 
 gulp.task('cssnano', function() {
-    return gulp.src(['css/**/*.css', '!css/**/*.min.css'])
+    return gulp.src(['./css/**/*.css', '!./css/**/*.min.css'])
         .pipe(rename({ suffix: '.min' }))
         .pipe(cssnano())
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('./css'));
 });
 
 gulp.task('uglify', function() {
-    return gulp.src(['js/**/*.js', '!js/**/*.min.js'])
+    return gulp.src(['./js/**/*.js', '!./js/**/*.min.js'])
         .pipe(rename({ suffix: '.min' }))
         .pipe(uglify())
-        .pipe(gulp.dest('js'));
+        .pipe(gulp.dest('./js'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('scss/**/*.scss', ['sass']);
-    gulp.watch(['css/**/*.css', '!css/**/*.min.css'], ['cssnano']);
-    gulp.watch(['js/**/*.js', '!js/**/*.min.js'], ['uglify']);
+    gulp.watch('./scss/**/*.scss', ['sass']);
+    gulp.watch(['./css/**/*.css', '!./css/**/*.min.css'], ['cssnano']);
+    gulp.watch(['./js/**/*.js', '!./js/**/*.min.js'], ['uglify']);
 });
 
 gulp.task('default', function() {
